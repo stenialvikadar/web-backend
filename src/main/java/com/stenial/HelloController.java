7a5fae2f8e00b57870a1e0ba587dfb3af03eeb6d
Bakir Sabic <bakir.sabic20b@gmail.com>
2025-01-17 13:16:19 +0100

    Added new controller

--- src/main/java/com/stenial/HelloController.java
+++ src/main/java/com/stenial/HelloController.java
@@ -1,14 +1,20 @@
 package com.stenial;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/bakir")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+	
+	@GetMapping("/bakir/abcs")
+	public String abcs(@RequestParam String myFirstParam) {
+		return "param: " + myFirstParam;
 	}
 
 }
